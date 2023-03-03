ecb23dbfb929b75a319aca2ebb75bd0e99824a8b
Menhi <dmitriy.zaruba@gmail.com>
2023-03-03 12:34:01 +0100

    1

---
+++ Task_manager/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Task{
+    string name;
+    string description;
+
+};
+
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    return 0;
+}
